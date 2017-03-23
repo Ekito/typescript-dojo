9fd699590fd49cf2b5a62f3447eec002f04704a9
jboulay <jboulay@ekito.fr>
2017-03-23 15:25:01 +0100

    Steps 1 to 8

--- src/string-calc.spec.ts
+++ src/string-calc.spec.ts
@@ -1,12 +1,45 @@
 import 'mocha';
-import { expect } from 'chai';
+import { expect, } from 'chai';
 
 import * as StringCalc from './string-calc';
 
 describe('HelloWorld', () => {
 
-    it('should say hello', () => {
-        const hello = StringCalc.sayHello('girl');
-        expect(hello).to.equals('Hello girl');
+    it('should return zero', () => {
+        const result = StringCalc.calc('');
+        expect(result).to.equals(0);
+    })
+
+
+    it('should return single number', () => {
+        const result = StringCalc.calc('42');
+        expect(result).to.equals(42);
+    })
+
+    it('should return the sum with comma delimited numbers', () => {
+        const result = StringCalc.calc('42,31');
+        expect(result).to.equals(73);
+    })
+
+    it('should return the sum with new line delimited numbers', () => {
+        const result = StringCalc.calc('42\n21');
+        expect(result).to.equals(63);
+    })
+
+    it('should return the sum with 3 numbers delimited either way', () => {
+        const result = StringCalc.calc('42\n21,3');
+        expect(result).to.equals(66);
+    })
+
+    it('should throw an exception with negative numbers', () => {
+        expect(() => StringCalc.calc('-423')).to.throw();
+    })
+
+    it('should ignore numbers > 1000', () => {
+        expect(StringCalc.calc('1,2,1200')).to.equal(3);
+    })
+
+    it('should define delimiter on first line', () => {
+        expect(StringCalc.calc('//#\n1#2')).to.equal(3);
     })
 })
--- src/string-calc.ts
+++ src/string-calc.ts
@@ -1,4 +1,15 @@
 
-export function sayHello(who: string){
-    return `Hello ${who}`;
+export function calc(inputString: string){
+     if (inputString === '') return 0;
+     let separators = ['\n', ','];
+     if (inputString.startsWith('//')) {
+        separators = [inputString.match(/\/\/(.)\n/)[1]];
+     }
+     let numbers = inputString.split(new RegExp(`[${separators.join()}]`)).map(x => parseInt(x));
+     numbers = numbers.filter(x => x<1000);
+     if (numbers.find(x => x<0))
+     {
+         throw new Error('No negative number');
+     }
+     return numbers.reduce((a, b) => a+b, 0);
 }
